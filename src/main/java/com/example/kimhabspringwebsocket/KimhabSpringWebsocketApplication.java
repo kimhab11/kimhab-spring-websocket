44c42149d3c12636e9e39491adbbd7e877c3c788
kimhab11 <keomhab@gmail.com>
2023-09-20 13:34:37 +0700

    Initial commit

---
+++ src/main/java/com/example/kimhabspringwebsocket/KimhabSpringWebsocketApplication.java
@@ -0,0 +1,13 @@
+package com.example.kimhabspringwebsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimhabSpringWebsocketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimhabSpringWebsocketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kimhabspringwebsocket/KimhabSpringWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kimhabspringwebsocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimhabSpringWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
